c71be69efdf851cadb66956f57b690b1086da815
Ravi Iyer <iyer.ravi@outlook.com>
2025-04-16 15:57:58 +0530

    Refactor main.py for MCP session handling and update dependencies
    - Replaced print statement with async context manager for MCP client session initialization.
    - Added loading of MCP tools and commented out agent creation logic.
    - Updated pyproject.toml to include langchain dependency and its version.
    - Added greenlet and sqlalchemy dependencies in uv.lock for compatibility.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import os
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
-from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
 
 
 load_dotenv()
@@ -20,7 +22,17 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write): #create stdio context manager with read+write objects
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session) #await session.list_tools()
+            print(tools)
+            #agent = create_react_agent(llm, tools)
+            # tools = load_mcp_tools(session)
+            # llm_with_tools = llm.bind_tools(tools)
+            # response = llm_with_tools.invoke("What is 2 + 2?")
+            # print(response)
 
 
 if __name__ == "__main__":
