b4077145c18c96dc06eaa0e91b1f918ce755bfef
Ravi Iyer <iyer.ravi@outlook.com>
2025-04-16 11:08:03 +0530

    Integrate MCP client and server parameters into main.py
    - Added imports for MCP client session and tools from langchain.
    - Initialized ChatOpenAI instance for language model integration.
    - Configured StdioServerParameters for a simple MCP server setup.

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import asyncio
 from dotenv import load_dotenv
 import os
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+
 
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    transport="stdio",
+    server_name="mcp-crash-course",
+    server_description="A simple MCP server for the crash course",
+    command="python3",
+    args=["/Users/raviiyer/dev/mcp-crash-course/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
